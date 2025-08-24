1ab67aab9d03c07097dda5ae239d35095f48119b
Jay Chakravarty <jaychakravarty@icloud.com>
2025-08-24 13:06:06 -0700

    Comment out DEBUG = True

--- capstone/settings.py
+++ capstone/settings.py
@@ -23,7 +23,7 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 SECRET_KEY = "django-insecure-((-^h9ej((z^97v_##-@gq(y45f(f!h()l$m#^mnq%524x27bh"
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+# DEBUG = True
 
 ALLOWED_HOSTS = [
     "127.0.0.1",
